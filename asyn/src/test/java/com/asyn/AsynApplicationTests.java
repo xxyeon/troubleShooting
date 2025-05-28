1e5917a222537ea8214afd7f800b51e9627e30f8
xxyeon <jykim9335@gmail.com>
2025-05-28 11:30:50 +0900

    test: supplyAsync로 스레드 할당을 기다리는 task 생성 - supplier에 2초 간격을 설정해서 확인 - 스레드가 여유로워서 바로 실행

--- asyn/src/test/java/com/asyn/AsynApplicationTests.java
+++ asyn/src/test/java/com/asyn/AsynApplicationTests.java
@@ -1,5 +1,10 @@
 package com.asyn;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +15,23 @@ class AsynApplicationTests {
 	void contextLoads() {
 	}
 
+
+	@Test
+	@DisplayName("주문 정보를 조회하는 예시입니다.")
+	void supplyAsync() throws Exception {
+		String orderNo = "1234567890";
+		CompletableFuture<String> orderInfoFuture = CompletableFuture.supplyAsync(() -> getOrderInfo(orderNo));
+
+		assertEquals("iPhone 15", orderInfoFuture.get()); // CompletableFuture.get() 호출로 비동기 작업이 시작되고 2초 뒤 결과 반환
+	}
+
+	private String getOrderInfo(String orderNo) {
+		try {
+			Thread.sleep(2000); // orderInfoRepository.findByOrderNo(orderNo);
+			System.out.println("2초후 실행됨");
+		} catch (InterruptedException e) {
+			// ..
+		}
+		return "iPhone 15";
+	}
 }
