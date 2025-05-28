5e6d75f16f334bc9edaa4ba136c99362f07f0b50
xxyeon <jykim9335@gmail.com>
2025-05-28 11:11:00 +0900

    init Project: 비동기 테스트

---
+++ asyn/src/main/java/com/asyn/AsynApplication.java
@@ -0,0 +1,13 @@
+package com.asyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsynApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsynApplication.class, args);
+	}
+
+}
